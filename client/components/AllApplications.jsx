c85e8892bcbbb6a1e5ef6e8fda9894cfeb305d2c
OkunoD <108437247+OkunoD@users.noreply.github.com>
2022-11-19 14:39:13 -0800

    completed more components and began building state management

--- client/components/AllApplications.jsx
+++ client/components/AllApplications.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Job from "./Job";
 import AppliedColumn from "./AppliedColumn";
 import FinalColumn from "./FinalColumn";
@@ -11,6 +12,32 @@ import TechnicalColumn from "./TechnicalColumn";
 const AllApplications = () => {
   //add functionality here
 
+  //add fetch request and set up initial state
+
+  const [applied, setApplied] = useState([]);
+  const [phone, setPhone] = useState([]);
+  const [technical, setTechnical] = useState([]);
+  const [final, setFinal] = useState([]);
+  const [offer, setOffer] = useState([]);
+  const [rejected, setRejected] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/')
+      .then(jobs => jobs.json())
+      .then((parsedJobs) => {
+        for (let i = 0; i < parsedJobs.length; i++) {
+          if (parsedJobs[i].status === 'applied') setApplied(...applied, parsedJobs[i]);
+          else if (parsedJobs[i].status === 'phone') setPhone(...phone, parsedJobs[i]);
+          else if (parsedJobs[i].status === 'technical') setTechnical(...technical, parsedJobs[i]);
+          else if (parsedJobs[i].status === 'offer') setOffer(...offer, parsedJobs[i]);
+          else if (parsedJobs[i].status === 'rejected') setRejected(...rejected, parsedJobs[i]);
+        }
+      })
+      .catch(err => console.log('Plants.componentDidMount: get plants: ERROR: ', err));
+  })
+
+ 
+
   return (
     <section className="mainSection">
         <Link to={'/AddApplication'}>
@@ -18,14 +45,12 @@ const AllApplications = () => {
             type="button"
             className="linkButton">Add New Job</button>
         </Link>
-        {/* 
-        <AppliedColumn />
-        <PhoneColumn />
-        <TechnicalColumn />
-        <FinalColumn />
-        <OfferColumn />
-        <RejectedColumn />
-        */}
+        <AppliedColumn appliedArr={state.applied}/>
+        <PhoneColumn phoneArr={state.phone}/>
+        <TechnicalColumn technicalArr={state.technical}/>
+        <FinalColumn finalArr={state.final}/>
+        <OfferColumn offerArr={state.offer}/>
+        <RejectedColumn rejectedArr={state.rejected}/>
       <h1>hello there</h1>
     </section>
   );
--- client/components/AppliedColumn.jsx
+++ client/components/AppliedColumn.jsx
@@ -1,6 +1,17 @@
 import React from "react";
+import { connect } from 'react-redux';
+
 
 const AppliedColumn = () => {
+
+  //  const appliedElems = state.map((applied, i) => {
+  //   return (
+  //     <AppliedColumn
+  //       key={i}
+  //       info={applied}
+  //     />
+  //   );
+  // });
     return (
       <h1>Applied</h1>
     )
